715088a201ebaa3147dbda0d04dc49e5a42293af
Jefferson Follmann <jeff.follmann@gmail.com>
2022-07-22 15:57:47 -0300

    Fix

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,55 @@
-// import { makeChangeOrderOutState } from './main/factories/states'
-// import { OrderOutEvent } from './domain/entities'
+import { makeChangeOrderOutState } from './main/factories/states'
+import { OrderOutEvent } from './domain/entities'
 
-import { RoleGuideEvent } from './domain/entities'
-import { makeChangeRoleGuideState } from './main/factories/states'
+// import { RoleGuideEvent } from './domain/entities'
+// import { makeChangeRoleGuideState } from './main/factories/states'
 
-// const runHappyPath = async () => {
-//   console.log('--- START HAPPY-PATH ---')
-//   const stateMachine = makeChangeOrderOutState()
-//   await stateMachine.handler(OrderOutEvent.OrderPaymentApproved)
-//   await stateMachine.handler(OrderOutEvent.OrderOutRegisterCreated)
-//   await stateMachine.handler(OrderOutEvent.OrderShipped)
-//   await stateMachine.handler(OrderOutEvent.OrderOutRegisterUpdated)
-//   await stateMachine.handler(OrderOutEvent.OrderShipmentDelivered)
+const runHappyPath = async () => {
+  console.log('--- START HAPPY-PATH ---')
+  const stateMachine = makeChangeOrderOutState()
+  await stateMachine.handler(OrderOutEvent.OrderPaymentApproved)
+  await stateMachine.handler(OrderOutEvent.OrderOutRegisterCreated)
+  await stateMachine.handler(OrderOutEvent.OrderShipped)
+  await stateMachine.handler(OrderOutEvent.OrderOutRegisterUpdated)
+  await stateMachine.handler(OrderOutEvent.OrderShipmentDelivered)
 
-//   console.log('--- END HAPPY-PATH ---\n')
-// }
-
-// const runRedeliveryPath = async () => {
-//   console.log('--- START REDELIVERY-PATH ---')
-//   const stateMachine = makeChangeOrderOutState(OrderOutEvent.OrderOutRegisterCreated)
-//   await stateMachine.handler(OrderOutEvent.OrderShipped)
-//   await stateMachine.handler(OrderOutEvent.OrderOutRegisterUpdated)
-//   await stateMachine.handler(OrderOutEvent.OrderShipmentDelivered)
-
-//   console.log('--- END REDELIVERY-PATH ---\n')
-// }
-
-// const runInvalidTransition = async () => {
-//   console.log('--- START INVALID-TRANSITION-PATH ---')
-//   try {
-//     const stateMachine = makeChangeOrderOutState(OrderOutEvent.OrderOutCreated)
-//     await stateMachine.handler(OrderOutEvent.OrderShipmentDelivered)
-//   } catch (error: any) {
-//     console.log(error.message)
-//   }
-
-//   console.log('--- END INVALID-TRANSITION-PATH ---\n')
-// }
-
-// const runOrderTests = async () => {
-//   await runHappyPath()
-//   await runRedeliveryPath()
-//   await runInvalidTransition()
-// }
-// runOrderTests()
-
-const runRoleGuideTestes = async () => {
-  const stateMachine = makeChangeRoleGuideState()
-  stateMachine.handler(RoleGuideEvent.Pleno)
-  stateMachine.handler(RoleGuideEvent.Senior)
-  stateMachine.handler(RoleGuideEvent.Especialista)
+  console.log('--- END HAPPY-PATH ---\n')
 }
 
-runRoleGuideTestes()
+const runRedeliveryPath = async () => {
+  console.log('--- START REDELIVERY-PATH ---')
+  const stateMachine = makeChangeOrderOutState(OrderOutEvent.OrderOutRegisterCreated)
+  await stateMachine.handler(OrderOutEvent.OrderShipped)
+  await stateMachine.handler(OrderOutEvent.OrderOutRegisterUpdated)
+  await stateMachine.handler(OrderOutEvent.OrderShipmentDelivered)
+
+  console.log('--- END REDELIVERY-PATH ---\n')
+}
+
+const runInvalidTransition = async () => {
+  console.log('--- START INVALID-TRANSITION-PATH ---')
+  try {
+    const stateMachine = makeChangeOrderOutState(OrderOutEvent.OrderOutCreated)
+    await stateMachine.handler(OrderOutEvent.OrderShipmentDelivered)
+  } catch (error: any) {
+    console.log(error.message)
+  }
+
+  console.log('--- END INVALID-TRANSITION-PATH ---\n')
+}
+
+const runOrderTests = async () => {
+  await runHappyPath()
+  await runRedeliveryPath()
+  await runInvalidTransition()
+}
+runOrderTests()
+
+// const runRoleGuideTestes = async () => {
+//   const stateMachine = makeChangeRoleGuideState()
+//   stateMachine.handler(RoleGuideEvent.Pleno)
+//   stateMachine.handler(RoleGuideEvent.Senior)
+//   stateMachine.handler(RoleGuideEvent.Especialista)
+// }
+
+// runRoleGuideTestes()
