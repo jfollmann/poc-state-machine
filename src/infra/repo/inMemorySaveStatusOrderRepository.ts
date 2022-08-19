eb17d9785e7a69a9459adc6274ea236499c456f5
Jefferson Follmann <jeff.follmann@gmail.com>
2022-08-19 15:02:36 -0300

    fix

--- src/infra/repo/inMemorySaveStatusOrderRepository.ts
+++ src/infra/repo/inMemorySaveStatusOrderRepository.ts
@@ -2,7 +2,7 @@ import { SaveOrderStatus } from '@/domain/repo'
 
 export class InMemorySaveStatusOrderRepository implements SaveOrderStatus {
   saveStatus (input: SaveOrderStatus.Input): Promise<boolean> {
-    console.log('[Repo Info]', input)
+    console.log('[Repository Info]', input)
     return Promise.resolve(true)
   }
 }
